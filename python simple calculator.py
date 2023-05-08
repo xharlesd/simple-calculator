97d714829e80d07a98b67513ae7ff1fba651aa84
xharlesd <bernidocharlesdavid@gmail.com>
2023-05-08 16:01:15 +0800

    pseudocode

---
+++ python simple calculator.py
@@ -0,0 +1,40 @@
+# Import the required libraries
+
+# Define a function to add numbers
+    # try
+    # catch
+
+# Define a function to subtract numbers
+    # try
+    # catch
+
+# Define a function to multiply numbers
+    # try
+    # catch
+
+# Define a function to divide numbers
+    # try
+    # catch
+
+# Define a function to clear the content of textbox 1 and textbox 2
+
+
+# Create an instance of tkinter frame or window
+# Title of the Window
+
+# Create a label for first number
+# Create a textbox for first number
+
+# Create a label for second number
+# Create a textbox for second number
+
+# Create label for Result
+
+# Create a button for addition of first and second number
+# Create a button for subtraction of first and second number
+# Create a button for multiplication of first and second number
+# Create a button for division of first and second number
+# Create a button for clearing the content of textbox 1 and textbox 2
+# Create an exit button
+
+# mainloop
